c2ab516a1ad6b8589a7794eb1cbc8ec63be50ddc
toskashala <shalatoska@gmail.com>
2025-11-20 16:13:43 +0100

    fixes

---
+++ studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'tt2ed1zq',  // Your project ID from sanity.config.js
+    dataset: 'production'   // Your dataset name
+  }
+})
--- studio/schemas/hero.js
+++ studio/schemas/hero.js
@@ -49,13 +49,5 @@ export default {
         },
       ],
     },
-    {
-      name: "backgroundImage",
-      title: "Background Image",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-    },
   ],
 };
